7d39676d8db5d78da87433536fe52c028c441647
Roni <roni.c.castro@gmail.com>
2020-05-10 22:17:47 -0300

    Create base Signup screen

---
+++ src/screens/Signup/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Signup: React.FC = () => {
+  return (
+    <Container>
+      <Text>Signup</Text>
+    </Container>
+  );
+};
+
+export default Signup;
