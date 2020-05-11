c49efe0bd2adc0a2797c3906cde70d4ee6f995a6
Roni <roni.c.castro@gmail.com>
2020-05-10 22:51:07 -0300

    Update styles

--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components/native';
+import { MediumText } from '../../constants/fontFamily';
 
 export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
   background-color: #312e38;
+`;
+
+export const Title = styled(MediumText)`
+  font-size: 24px;
+  color: #f4ede8;
+  margin: 64px 0px 24px;
 `;
