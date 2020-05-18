8930a5dcdca2ca97458361148af85bf371059a16
Roni <roni.c.castro@gmail.com>
2020-05-18 00:05:19 -0300

    Add AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import AuthRoutes from './routes';
+import AppProvider from './hooks/AppProvider';
 import { NavigationContainer } from '@react-navigation/native';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312E38" />
-      <AuthRoutes />
+      <AppProvider>
+        <AuthRoutes />
+      </AppProvider>
     </NavigationContainer>
   );
 };
