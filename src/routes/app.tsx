003c1c1c17ede351d101360ec9763cffff5fb4bd
Roni <roni.c.castro@gmail.com>
2020-07-19 23:43:40 -0300

    Add safearea to all screens

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -4,25 +4,29 @@ import Dashboard from '../screens/Dashboard';
 import CreateAppointment from '../screens/CreateAppointment';
 import AppointmentCreated from '../screens/AppointmentCreated';
 import Profile from '../screens/Profile';
+import { SafeAreaView, StatusBar } from 'react-native';
 
 const App = createStackNavigator();
 
 const AppRoutes: React.FC = () => {
   return (
-    <App.Navigator
-      screenOptions={{
-        headerShown: false,
-        cardStyle: {
-          backgroundColor: '#312e38',
-        },
-      }}
-    >
-      <App.Screen name="Dashboard" component={Dashboard} />
-      <App.Screen name="CreateAppointment" component={CreateAppointment} />
-      <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#28262e' }}>
+      <StatusBar backgroundColor="#28262e" />
+      <App.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: {
+            backgroundColor: '#312e38',
+          },
+        }}
+      >
+        <App.Screen name="Dashboard" component={Dashboard} />
+        <App.Screen name="CreateAppointment" component={CreateAppointment} />
+        <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
 
-      <App.Screen name="Profile" component={Profile} />
-    </App.Navigator>
+        <App.Screen name="Profile" component={Profile} />
+      </App.Navigator>
+    </SafeAreaView>
   );
 };
 
