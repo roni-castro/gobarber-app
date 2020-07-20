2b714c8fc239640106b60a865faa4c0d78b38b34
Roni <roni.c.castro@gmail.com>
2020-07-20 00:09:06 -0300

    Add authentication header to all requests

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,7 +1,25 @@
 import axios from 'axios';
+import { getStorageItem } from '../utils/storage';
+import { TOKEN } from './auth/authStorageConstants';
 
 const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
+
+api.interceptors.request.use(
+  async function (config) {
+    const token = await getStorageItem<string>(TOKEN);
+
+    if (token != null) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+
+  function (err) {
+    return Promise.reject(err);
+  },
+);
 
 export default api;
