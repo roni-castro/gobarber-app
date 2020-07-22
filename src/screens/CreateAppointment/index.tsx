a13de9f97828556cf5caa27a7e37384ade2dc1f5
Roni <roni.c.castro@gmail.com>
2020-07-21 22:39:07 -0300

    Add header to create appointment screen

--- src/screens/CreateAppointment/index.tsx
+++ src/screens/CreateAppointment/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
-
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  ProviderAvatar,
+} from './styles';
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  return (
+    <Container>
+      <Header>
+        <BackButton>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabelereiros</HeaderTitle>
+        <ProviderAvatar
+          source={{
+            uri: 'https://avatars3.githubusercontent.com/u/24610813?v=4',
+          }}
+        />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
--- src/screens/CreateAppointment/styles.ts
+++ src/screens/CreateAppointment/styles.ts
@@ -1,3 +1,36 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 24px;
+  background-color: #28262e;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  width: 40px;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HeaderTitle = styled.Text`
+  flex: 1;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Medium';
+  line-height: 28px;
+  color: #f4ede8;
+  margin-left: 8px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 58px;
+  height: 58px;
+  border-radius: 29px;
+  margin-left: 10px;
+  background-color: red;
+`;
