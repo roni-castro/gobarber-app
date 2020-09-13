a3597c646215df34e8a03e56114cbce9de2bc67b
Roni <roni.c.castro@gmail.com>
2020-09-12 23:42:42 -0300

    Fix style of signup

--- src/screens/Signup/styles.ts
+++ src/screens/Signup/styles.ts
@@ -1,11 +1,10 @@
 import styled from 'styled-components/native';
-import { Platform } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  margin: 0 30px ${Platform.OS === 'android' ? 120 : 100}px 30px;
+  margin: 0 30px 50px 30px;
 `;
 
 export const Title = styled.Text`
